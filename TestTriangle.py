a8d313b334f621a187165a066bb9fef8dac8be2d
Tim Tham <90109466+TWT1989@users.noreply.github.com>
2021-10-14 13:35:48 -0400

    Updated to Test Updated Code from Code Analysis

--- TestTriangle.py
+++ TestTriangle.py
@@ -9,40 +9,40 @@ The primary goal of this file is to demonstrate a simple unittest implementation
 
 import unittest
 
-from Triangle import classifyTriangle
+from triangle import classify_triangle
 
 # This code implements the unit test functionality
 # https://docs.python.org/3/library/unittest.html has a nice description of the framework
 
-class TestTriangle(unittest.TestCase):
+class Testtriangle(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
 
     def testRightTriangle(self): 
-        self.assertEqual(classifyTriangle(3,4,5),'Right')
+        self.assertEqual(classify_triangle(3,4,5),'Right')
         
     def testIsoscelesTriangle(self): 
-        self.assertEqual(classifyTriangle(3,3,4),'Isosceles')
+        self.assertEqual(classify_triangle(3,3,4),'Isosceles')
                
     def testEquilateralTriangle(self): 
-        self.assertEqual(classifyTriangle(1,1,1),'Equilateral')
+        self.assertEqual(classify_triangle(1,1,1),'Equilateral')
         
     def testScaleneTriangle(self): 
-        self.assertEqual(classifyTriangle(4,7,9),'Scalene')
+        self.assertEqual(classify_triangle(4,7,9),'Scalene')
         
     def testInvalidInputInteger(self): 
-        self.assertEqual(classifyTriangle(4,7,9.9),'InvalidInput')
+        self.assertEqual(classify_triangle(4,7,9.9),'InvalidInput')
         
     def testInvalidInputNegativeValue(self): 
-        self.assertEqual(classifyTriangle(4,-7,9),'InvalidInput')
+        self.assertEqual(classify_triangle(4,-7,9),'InvalidInput')
         
     def testInvalidInputZeroValue(self): 
-        self.assertEqual(classifyTriangle(0,7,9),'InvalidInput')
+        self.assertEqual(classify_triangle(0,7,9),'InvalidInput')
         
     def testInvalidInputOutOfBounds(self): 
-        self.assertEqual(classifyTriangle(400,700,900),'InvalidInput')
+        self.assertEqual(classify_triangle(400,700,900),'InvalidInput')
         
     def testNotATriangle(self): 
-        self.assertEqual(classifyTriangle(7,4,1),'NotATriangle')
+        self.assertEqual(classify_triangle(7,4,1),'NotATriangle')
         
 
 if __name__ == '__main__':
